8aeb0ed6bfa3dfccc73d1af80520e21e21501a63
Craig Freeman <craigfreeman@gmail.com>
2020-05-16 14:09:04 -0600

    Initial

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,6 +1,6 @@
-const serverlessHttp = require("serverless-http");
+const serverlessHttp = require('serverless-http');
 
-const app = require("./index.js");
+const app = require('./bundle/index.js');
 
 module.exports.handler = serverlessHttp(app, {
   /**
@@ -13,5 +13,5 @@ module.exports.handler = serverlessHttp(app, {
   request(req, event, context) {
     req.event = event;
     req.context = context;
-  }
+  },
 });
